8180e64295757e74f24597681b72fe4547124d0c
geptestuser <geptestuser@MUM02D5553>
2017-07-13 15:15:30 +0530

    Add project files.

---
+++ MVC_DemoDRUDUsingEF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_DemoDRUDUsingEF.Startup))]
+namespace MVC_DemoDRUDUsingEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
